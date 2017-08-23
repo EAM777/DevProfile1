50782a1faf52d029134c6ab07152ccb0a817f1db
Elliot <elliot@MacBook-Pro.local>
2017-08-22 20:15:16 -0700

    Build User Interphase

---
+++ DevProfile1/DevProfile1/View/RadiusMod.swift
@@ -0,0 +1,28 @@
+//
+//  RadiusMod.swift
+//  DevProfile1
+//
+//  Created by Elliot on 8/22/17.
+//  Copyright © 2017 Elliot. All rights reserved.
+//
+
+import UIKit
+
+class RadiusMod: UIImageView {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.cornerRadius = 7.7
+    }
+    
+    
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
